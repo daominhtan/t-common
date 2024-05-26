d280c80b52b9e209edf816f4f463932d85c00efe
tandao <>
2024-05-26 11:32:49 +0700

    Initial code

---
+++ src/main/java/com/tan/t_common/TCommonApplication.java
@@ -0,0 +1,13 @@
+package com.tan.t_common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TCommonApplication.class, args);
+	}
+
+}
